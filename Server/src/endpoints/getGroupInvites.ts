ccf971508c709ec60427bb52c5ef0f117f7696b8
PictureCo <90827823+abrandalik@users.noreply.github.com>
2024-05-30 22:58:27 +0200

    gg

--- Server/src/endpoints/getGroupInvites.ts
+++ Server/src/endpoints/getGroupInvites.ts
@@ -7,24 +7,35 @@ import { groupInteface } from '../models/modules.js';
 export async function geGroupInvites(req:express.Request, res :express.Response) {
     try{
         const Group = await group.findById(req.params.groupId)
-        let InviteList = []
         if (Group){
-            for (let index = 0; index < Group.invitedUsers.length; index++) {
-                const element = Group.invitedUsers[index];
-                const Invite = await invite.findById(req.body.Id).populate<{ invited: userInterface }>("invited")
-                let NameId = {
-                    inviteId: Invite?._id.id,
-                    UserName: Invite?.invited.userName,
+            for (let index = 0; index < Group.members.length; index++){
+                const element = Group.members[index];
+                if(element._id?.toHexString() == req.body.id){
+
+                
+                    
+                    let InviteList = []
+                    
+                        for (let index = 0; index < Group.invitedUsers.length; index++) {
+                            const element = Group.invitedUsers[index];
+                            const Invite = await invite.findById(req.body.Id).populate<{ invited: userInterface }>("invited")
+                            let NameId = {
+                                inviteId: Invite?._id.id,
+                                UserName: Invite?.invited.userName,
+
+                            }
+                            InviteList.push(NameId)
+                        }
+                        res.status(200).json(InviteList)
+                        
 
                 }
-                InviteList.push(NameId)
+                
             }
-            res.status(200).json(InviteList)
+
         }else{
-            res.status(404).send("user not found")
+            res.status(404).send("group not found")
         }
-        
-        
     }catch(error){
         res.status(500).send("internal error")
     }
